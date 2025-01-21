f038efc6809da9f20cae4367a551c883f41c1176
Omarespo-dev <omaresposito21@gmail.com>
2025-01-21 14:05:20 +0100

    finish snack 1-2-3-4 add another 4

--- snack-4/script.js
+++ snack-4/script.js
@@ -9,8 +9,24 @@ const posts = [
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
 
 // DICHIARO NUOVO ARRAY DOVE ANDRANNO I NOMI 
+// USO MAP PERCHE DEVO CREARE UN NUOVO ARRAY DOVE ANDRANNO SOLO I NOMI 
 const newArrayNomi = posts.map(function (nomi){
   return nomi.author
 })
 
 console.log(newArrayNomi)
+
+
+
+
+// VERSIONE CICLO FOR
+// let newArrayNomi = []
+
+// for(let i = 0; i < posts.length; i++){
+//   // variabile iesima
+//   let nomIesima = posts[i]
+//   // FACCIO PUSH NEL ARRAY VUOTO E PRENDO I NOMI DA NOMiESIMA
+//   newArrayNomi.push(nomIesima.author)
+// }
+
+// console.log(newArrayNomi)
